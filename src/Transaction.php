f27d2164909c6c5885a292462afde5375182d585
sc0Vu <alk03073135@gmail.com>
2018-02-21 23:19:23 +0800

    Get function of transaction data.

---
+++ src/Transaction.php
@@ -0,0 +1,186 @@
+<?php
+
+/**
+ * This file is part of ethereum-tx package.
+ * 
+ * (c) Kuan-Cheng,Lai <alk03073135@gmail.com>
+ * 
+ * @author Peter Lai <alk03073135@gmail.com>
+ * @license MIT
+ */
+
+namespace EthereumTx;
+
+use InvalidArgumentException;
+use RuntimeException;
+use RLP\RLP;
+use Secp256k1\Secp256k1;
+use ArrayAccess;
+
+class Transaction implements ArrayAccess
+{
+    /**
+     * txData
+     * 
+     * @var array
+     */
+    protected $map = [
+        'nonce' => 0,
+        'gasPrice' => 1,
+        'gasLimit' => 2,
+        'gas' => 2,
+        'to' => 3,
+        'value' => 4,
+        'data' => 5,
+        'chainId' => 6,
+        // 'r' => 7,
+        // 's' => 8
+    ];
+
+    /**
+     * txData
+     * 
+     * @var array
+     */
+    protected $txData;
+
+    /**
+     * construct
+     * 
+     * @param array $txData
+     * @return void
+     */
+    public function __construct(array $txData=[])
+    {
+        foreach ($txData as $key => $data) {
+            $txKey = isset($this->map[$key]) ? $this->map[$key] : null;
+
+            if (is_int($txKey)) {
+                $this->txData[$txKey] = $data;
+            }
+        }
+    }
+
+    /**
+     * get
+     * 
+     * @param string $name
+     * @return mixed
+     */
+    public function __get($name)
+    {
+        $method = 'get' . ucfirst($name);
+
+        if (method_exists($this, $method)) {
+            return call_user_func_array([$this, $method], []);
+        }
+        return $this->offsetGet($name);
+    }
+
+    /**
+     * set
+     * 
+     * @param string $name
+     * @param mixed $value
+     * @return void
+     */
+    public function __set($name, $value)
+    {
+        $method = 'set' . ucfirst($name);
+
+        if (method_exists($this, $method)) {
+            return call_user_func_array([$this, $method], [$value]);
+        }
+        return $this->offsetSet($name, $value);
+    }
+
+    /**
+     * offsetSet
+     * 
+     * @param string $offset
+     * @param string $value
+     * @return void
+     */
+    public function offsetSet($offset, $value)
+    {
+        $txKey = isset($this->map[$offset]) ? $this->map[$offset] : null;
+
+        if (is_int($txKey)) {
+            $this->txData[$txKey] = $value;
+        }
+    }
+
+    /**
+     * offsetExists
+     * 
+     * @param string $offset
+     * @return bool
+     */
+    public function offsetExists($offset)
+    {
+        $txKey = isset($this->map[$offset]) ? $this->map[$offset] : null;
+
+        if (is_int($txKey)) {
+            return isset($this->txData[$txKey]);
+        }
+        return false;
+    }
+
+    /**
+     * offsetUnset
+     * 
+     * @param string $offset
+     * @return void
+     */
+    public function offsetUnset($offset)
+    {
+        $txKey = isset($this->map[$offset]) ? $this->map[$offset] : null;
+
+        if (is_int($txKey) && isset($this->txData[$txKey])) {
+            unset($this->txData[$txKey]);
+        }
+    }
+
+    /**
+     * offsetGet
+     * 
+     * @param string $offset
+     * @return mixed
+     */
+    public function offsetGet($offset)
+    {
+        $txKey = isset($this->map[$offset]) ? $this->map[$offset] : null;
+
+        if (is_int($txKey) && isset($this->txData[$txKey])) {
+            return $this->txData[$txKey];
+        }
+        return null;
+    }
+
+    /**
+     * getTxData
+     * 
+     * @return array
+     */
+    public function getTxData()
+    {
+        return $this->txData;
+    }
+
+    /**
+     * sign
+     * 
+     * @param string $privateKey
+     * @return string
+     */
+    public function sign($privateKey)
+    {}
+
+    /**
+     * hash
+     * 
+     * @return string
+     */
+    protected function hash()
+    {}
+}
---
+++ test/unit/TransactionTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Test\Unit;
+
+use Test\TestCase;
+use EthereumTx\Transaction;
+
+class TransactionTest extends TestCase
+{
+    /**
+     * testGet
+     * 
+     * @return void
+     */
+    public function testGet()
+    {
+        $transaction = new Transaction([
+            'from' => '0xb60e8dd61c5d32be8058bb8eb970870f07233155',
+            'to' => '0xd46e8dd67c5d32be8058bb8eb970870f07244567',
+            'gas' => '0x76c0',
+            'gasPrice' => '0x9184e72a000',
+            'value' => '0x9184e72a',
+            'data' => '0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675'
+        ]);
+
+        $this->assertEquals(null, $transaction['from']);
+        $this->assertEquals('0xd46e8dd67c5d32be8058bb8eb970870f07244567', $transaction['to']);
+        $this->assertEquals('0x76c0', $transaction['gas']);
+        $this->assertEquals('0x9184e72a000', $transaction['gasPrice']);
+        $this->assertEquals('0x9184e72a', $transaction['value']);
+        $this->assertEquals('0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675', $transaction['data']);
+        $this->assertEquals(null, $transaction['chainId']);
+
+        $this->assertEquals(null, $transaction->from);
+        $this->assertEquals('0xd46e8dd67c5d32be8058bb8eb970870f07244567', $transaction->to);
+        $this->assertEquals('0x76c0', $transaction->gas);
+        $this->assertEquals('0x9184e72a000', $transaction->gasPrice);
+        $this->assertEquals('0x9184e72a', $transaction->value);
+        $this->assertEquals('0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675', $transaction->data);
+        $this->assertEquals(null, $transaction->chainId);
+    }
+
+    /**
+     * testSign
+     * 
+     * @return void
+     */
+    public function testSign()
+    {
+        $this->assertTrue(true);
+    }
+}

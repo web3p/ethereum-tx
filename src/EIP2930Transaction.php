20b622e1e558f14bdf6657cfd6348b408c40c057
sc0vu <alk03073135@gmail.com>
2021-08-29 04:50:04 +0000

    Update EIP2930 transaction

--- src/EIP2930Transaction.php
+++ src/EIP2930Transaction.php
@@ -26,7 +26,7 @@ use Web3p\EthereumUtil\Util;
  * use Web3p\EthereumTx\EIP2930Transaction;
  * 
  * // generate transaction instance with transaction parameters
- * $transaction = new Transaction([
+ * $transaction = new EIP2930Transaction([
  *     'nonce' => '0x01',
  *     'from' => '0xb60e8dd61c5d32be8058bb8eb970870f07233155',
  *     'to' => '0xd46e8dd67c5d32be8058bb8eb970870f07244567',
@@ -39,7 +39,7 @@ use Web3p\EthereumUtil\Util;
  * ]);
  * 
  * // generate transaction instance with hex encoded transaction
- * $transaction = new Transaction('0xf86c098504a817c800825208943535353535353535353535353535353535353535880de0b6b3a76400008025a028ef61340bd939bc2195fe537567866003e1a15d3c71ff63e1590620aa636276a067cbe9d8997f761aecb703304b3800ccf555c9f3dc64214b297fb1966a3b6d83');
+ * $transaction = new EIP2930Transaction('0x01f86604158504a817c8008252089435353535353535353535353535353535353535358080c001a09753969d39f6a5109095d5082d67fc99a05fd66a339ba80934504ff79474e77aa07a907eb764b72b3088a331e7b97c2bad5fd43f1d574ddc80edeb022476454adb');
  * ```
  * 
  * ```php

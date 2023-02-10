8f89d8bc073a298980a2f3e85caef53930cb7ea8
David Cochrum <david.cochrum@transium.io>
2023-02-10 17:52:31 -0500

    Update to match `ArrayAccess` interface for PHP 8.0

--- src/TypeTransaction.php
+++ src/TypeTransaction.php
@@ -210,9 +210,10 @@ class TypeTransaction implements ArrayAccess
         $method = 'set' . ucfirst($name);
 
         if (method_exists($this, $method)) {
-            return call_user_func_array([$this, $method], [$value]);
+            call_user_func_array([$this, $method], [$value]);
+            return;
         }
-        return $this->offsetSet($name, $value);
+        $this->offsetSet($name, $value);
     }
 
     /**
@@ -227,12 +228,12 @@ class TypeTransaction implements ArrayAccess
 
     /**
      * Set the value in the transaction with given key.
-     * 
-     * @param string $offset key, eg: to
-     * @param string value
+     *
+     * @param mixed $offset key, eg: to
+     * @param mixed $value
      * @return void
      */
-    public function offsetSet($offset, $value)
+    public function offsetSet(mixed $offset, mixed $value): void
     {
         $txKey = isset($this->attributeMap[$offset]) ? $this->attributeMap[$offset] : null;
 
@@ -293,11 +294,11 @@ class TypeTransaction implements ArrayAccess
 
     /**
      * Return whether the value is in the transaction with given key.
-     * 
-     * @param string $offset key, eg: to
+     *
+     * @param mixed $offset key, eg: to
      * @return bool
      */
-    public function offsetExists($offset)
+    public function offsetExists(mixed $offset): bool
     {
         $txKey = isset($this->attributeMap[$offset]) ? $this->attributeMap[$offset] : null;
 
@@ -309,11 +310,11 @@ class TypeTransaction implements ArrayAccess
 
     /**
      * Unset the value in the transaction with given key.
-     * 
-     * @param string $offset key, eg: to
+     *
+     * @param mixed $offset key, eg: to
      * @return void
      */
-    public function offsetUnset($offset)
+    public function offsetUnset(mixed $offset): void
     {
         $txKey = isset($this->attributeMap[$offset]) ? $this->attributeMap[$offset] : null;
 
@@ -324,11 +325,11 @@ class TypeTransaction implements ArrayAccess
 
     /**
      * Return the value in the transaction with given key.
-     * 
-     * @param string $offset key, eg: to 
+     *
+     * @param mixed $offset key, eg: to
      * @return mixed value of the transaction
      */
-    public function offsetGet($offset)
+    public function offsetGet(mixed $offset): mixed
     {
         $txKey = isset($this->attributeMap[$offset]) ? $this->attributeMap[$offset] : null;
 
